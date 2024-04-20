2d9ad137e61f875240c8d3dd043e46e262f98f53
Sameer <115874777+shelarsameer@users.noreply.github.com>
2024-04-20 19:24:22 +0530

    Add files via upload

---
+++ src/components/Home/Head.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Head = () => {
+  return (
+    <div className="head-row">
+      <div className="col-2">
+        <h1>GYMWHALE</h1>
+        <h3>
+        Elevate your Lifestyle!!!
+        </h3>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <a href="" className="btn">
+          Explore&nbsp;&nbsp;&#8594;
+        </a>
+      </div>
+      <div className="col-2">
+        <img className="main-img" src="src/Assets/category-4.jpg.jpg" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
